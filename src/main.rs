8be3afdbd9d42ac01baf26393b3e28b67df3cf4e
Drew Harris <61394759+drew-harris@users.noreply.github.com>
2022-05-07 21:20:16 -0500

    Change colors

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
-use btleplug::{platform::{Manager, Adapter, Peripheral}, api::{Manager as _, Central, ScanFilter, Peripheral as _, bleuuid::BleUuid}};
+use btleplug::{
+    api::{bleuuid::BleUuid, Central, Manager as _, Peripheral as _, ScanFilter},
+    platform::{Adapter, Manager, Peripheral},
+};
 use std::{error::Error, time::Duration};
 use tokio::time;
 
-
-enum LightCommand {
-    TurnOn()
-}
-
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-
     println!("Scanning devices");
     let manager = Manager::new().await.unwrap();
 
@@ -24,7 +20,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     time::sleep(Duration::from_secs(1)).await;
 
     for p in central.peripherals().await.unwrap() {
-        let name  = p.properties().await.unwrap().unwrap().local_name;
+        let name = p.properties().await.unwrap().unwrap().local_name;
         match name {
             Some(name) => println!("{}", name),
             None => (),
@@ -33,8 +29,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let light = find_light(&central).await.unwrap();
 
-
-    println!("Connecting to {}", light.properties().await.unwrap().unwrap().local_name.unwrap());
+    println!(
+        "Connecting to {}",
+        light
+            .properties()
+            .await
+            .unwrap()
+            .unwrap()
+            .local_name
+            .unwrap()
+    );
     light.connect().await.unwrap();
     println!("Connected");
     light.discover_services().await?;
@@ -42,37 +46,50 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let chars = light.characteristics();
     for charis in chars.iter() {
         println!("{}", &charis.uuid.to_short_string());
-        let off_cmd = vec![0x33, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x32];
-
-        let keep_alive = vec![0xAA, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xAB];
-
-        light.write(&charis, &keep_alive, btleplug::api::WriteType::WithoutResponse).await;
-        light.write(&charis, &off_cmd, btleplug::api::WriteType::WithoutResponse).await;
     }
 
-    //let char_cmd = chars.iter().find(|c| c.uuid.to_short_string() == "00010203-0405-0607-0809-0a0b0c0d2b11".to_string()).unwrap();
+    let char_cmd = chars
+        .iter()
+        .find(|c| c.uuid.to_short_string() == "00010203-0405-0607-0809-0a0b0c0d2b11".to_string())
+        .unwrap();
 
-    //let off_cmd = vec![0x33, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x32];
+    let mut off_cmd: Vec<u8> = vec![0x33, 0x01, 0x00];
+    let mut red: Vec<u8> = vec![0x33, 0x05, 0x02, 244, 0, 0];
+    fill_and_sum(&mut off_cmd);
+    fill_and_sum(&mut red);
 
-    //let keep_alive = vec![0xAA, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xAB];
-
-    //let result = light.write(&char_cmd, &keep_alive, btleplug::api::WriteType::WithoutResponse).await;
-    //let result = light.write(&char_cmd, &off_cmd, btleplug::api::WriteType::WithoutResponse).await;
-    //match result {
-        //Ok(()) => println!("Command sent"),
-        //Err(err) => (),
-    //}
-
-
+    let result = light
+        .write(
+            &char_cmd,
+            &red,
+            btleplug::api::WriteType::WithoutResponse,
+        )
+        .await;
+    match result {
+        Ok(()) => println!("Command sent"),
+        Err(err) => (),
+    }
 
     time::sleep(Duration::from_secs(3)).await;
-
 
     light.disconnect().await?;
     println!("Disconnected");
 
     Ok(())
+}
 
+fn fill_and_sum(inputCmd: &mut Vec<u8>) {
+    // Zeropad and then add an XOR checksum
+    while inputCmd.len() < 19 {
+        inputCmd.push(0x00);
+    }
+
+    let mut sum = 0;
+    for i in inputCmd.iter() {
+        sum = sum ^ i;
+    }
+    inputCmd.push(sum);
+    println!("Command IS: {}", hex::encode(inputCmd));
 }
 
 async fn find_light(central: &Adapter) -> Option<Peripheral> {
@@ -83,7 +100,7 @@ async fn find_light(central: &Adapter) -> Option<Peripheral> {
             .unwrap()
             .local_name
             .iter()
-            .any(|name| name.contains("48EA"))
+            .any(|name| name.contains("6146"))
         {
             return Some(p);
         }
