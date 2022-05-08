29075ac311b52c5024878de664236ef39154d55d
Drew Harris <61394759+drew-harris@users.noreply.github.com>
2022-05-07 20:18:05 -0500

    Flash rainbow

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-use btleplug::{platform::{Manager, Adapter, Peripheral}, api::{Manager as _, Central, ScanFilter, Peripheral as _}};
+use btleplug::{platform::{Manager, Adapter, Peripheral}, api::{Manager as _, Central, ScanFilter, Peripheral as _, bleuuid::BleUuid}};
 use std::{error::Error, time::Duration};
 use tokio::time;
+
+
+enum LightCommand {
+    TurnOn()
+}
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -26,6 +32,44 @@ async fn main() -> Result<(), Box<dyn Error>> {
     }
 
     let light = find_light(&central).await.unwrap();
+
+
+    println!("Connecting to {}", light.properties().await.unwrap().unwrap().local_name.unwrap());
+    light.connect().await.unwrap();
+    println!("Connected");
+    light.discover_services().await?;
+    println!("Discovering services...");
+    let chars = light.characteristics();
+    for charis in chars.iter() {
+        println!("{}", &charis.uuid.to_short_string());
+        let off_cmd = vec![0x33, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x32];
+
+        let keep_alive = vec![0xAA, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xAB];
+
+        light.write(&charis, &keep_alive, btleplug::api::WriteType::WithoutResponse).await;
+        light.write(&charis, &off_cmd, btleplug::api::WriteType::WithoutResponse).await;
+    }
+
+    //let char_cmd = chars.iter().find(|c| c.uuid.to_short_string() == "00010203-0405-0607-0809-0a0b0c0d2b11".to_string()).unwrap();
+
+    //let off_cmd = vec![0x33, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x32];
+
+    //let keep_alive = vec![0xAA, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xAB];
+
+    //let result = light.write(&char_cmd, &keep_alive, btleplug::api::WriteType::WithoutResponse).await;
+    //let result = light.write(&char_cmd, &off_cmd, btleplug::api::WriteType::WithoutResponse).await;
+    //match result {
+        //Ok(()) => println!("Command sent"),
+        //Err(err) => (),
+    //}
+
+
+
+    time::sleep(Duration::from_secs(3)).await;
+
+
+    light.disconnect().await?;
+    println!("Disconnected");
 
     Ok(())
 
